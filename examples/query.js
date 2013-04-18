ece1c97ea329a681d1218f7345e3e3e4311b7d62
Simon Oberhammer <simon@nekapuzer.at>
2013-04-18 20:49:10 +0200

    make singleton visible to user by handing him datasource and not the connection

--- examples/query.js
+++ examples/query.js
@@ -1,21 +1,24 @@
 var sql = require('sql-ringojs-client');
 
-sql.connect('jdbc:mysql://localhost/hello_world', 'com.mysql.jdbc.Driver', 'benchmarkdbuser', 'benchmarkdbpass');
+var datasource = module.singleton('pooling-source', function() {
+	return sql.connect('jdbc:mysql://localhost/hello_world', 'com.mysql.jdbc.Driver', 'benchmarkdbuser', 'benchmarkdbpass');
+});
 
 // `close()` returns the connection to the pool.
 // you must make sure that the connection is returned to the pool once
-// and only once. 
+// and only once.
 
 try {
-   var connection = sql.getConnection();
+   var connection = datasource.getConnection();
    var result = sql.query(connection, 'SELECT * FROM World WHERE id=2');
 } catch (e) {
-   connection.close();
    console.error(e);
+   connection.close();
    connection = null;
 } finally {
    if (connection) {
       connection.close();
    }
 }
-  
+
+console.dir(result);
